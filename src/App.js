a3ce3a02adf60931f950a28f2fe8af0776994ffc
Suchitra Naik <susanaik@gmail.com>
2018-10-24 18:10:27 +0000

    automatically backed up by learn

--- src/App.js
+++ src/App.js
@@ -1,5 +1,10 @@
 import React from 'react'
 class App extends React.Component {
   componentDidMount() = {this.handleAddTimer}
-
+  render() {
+    return (
+      <div>
+        <Timer />
+      </div>
+    )}
 }
